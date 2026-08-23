db2c08f89e4de3c1370b7f1188a8796ab8d3e54d
Bernice Boateng <berniceboateng775@gmail.com>
2026-08-23 09:09:25 +0000

    feat: implement responsive navigation component and global dark mode styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   return (
     <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <div className="container navbar-content">
-        <a href="/" className="nav-logo"></a>
+
 
         {/* Desktop Navigation */}
         <div className="nav-links">
